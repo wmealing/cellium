b2e473e8197a27c19988425fac38f077482b7a66
Wade Mealing <wmealing@gmail.com>
2025-10-24 21:03:39 +1000

    Actually include the demo widget called frame, that created this example.

---
+++ apps/cellium/src/frame.erl
@@ -0,0 +1,36 @@
+-module(frame).
+
+
+-export([render/1, new/1]).
+
+-include("cellium.hrl").
+
+
+new(Id) ->
+    (widget:new())#{id => Id,
+                    widget_type => visible_container,
+                    padding => #{top => 1, bottom => 1, left => 1, right => 1},
+                    type => container }.
+
+render(Widget) ->
+
+    X = maps:get(x, Widget, 0),
+    Y = maps:get(y, Widget, 0),
+
+    Width = maps:get(width, Widget, 0),
+    Height = maps:get(height, Widget, 0),
+
+    Bg = maps:get('background-color', Widget, black),
+    Fg = maps:get(color, Widget, white),
+
+    FrameTitle = <<"[ hello ]">>,
+
+    [?TERMBOX:tb_set_cell(X1, Y1, $_, 1, 2) || X1 <- lists:seq(X, X + Width),
+                                               Y1 <- lists:seq(Y, Y + Height)],
+
+    Box = box_styles:double(),
+
+    % okay so now we draw that table
+    table:draw_table(X,Y, Height, Fg,Bg, Box, [Width -1]),
+    ?TERMBOX:tb_print(X + 2, Y, Fg, Bg, FrameTitle),
+    ok.
