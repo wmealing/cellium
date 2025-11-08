5833fb56923a593e38db9d1dd7a88e693127741c
Wade Mealing <wmealing@gmail.com>
2025-11-08 16:42:29 +1000

    Fix frame width rendering now that i have the layout working more correctly.

--- apps/cellium/src/frame.erl
+++ apps/cellium/src/frame.erl
@@ -37,21 +37,20 @@ new(Id) ->
 %%% @end
 -spec render(map()) -> ok.
 render(Widget) ->
-
     #{x := X, y := Y, fg := Fg, bg := Bg} = get_common_props(Widget),
 
     Width = maps:get(width, Widget, 0),
     Height = maps:get(height, Widget, 0),
 
-    FrameTitle = maps:get(text, Widget, <<"Untitled">>),
+    if Width > 1 andalso Height > 1 ->
+        FrameTitle = maps:get(text, Widget, <<"Untitled">>),
 
+        [?TERMBOX:tb_set_cell(X1, Y1, $_, 1, 2) || X1 <- lists:seq(X, X + Width - 1),
+                                                   Y1 <- lists:seq(Y, Y + Height - 1)],
+        Box = box_styles:double(),
 
-    [?TERMBOX:tb_set_cell(X1, Y1, $_, 1, 2) || X1 <- lists:seq(X, X + Width),
-                                               Y1 <- lists:seq(Y, Y + Height)],
-
-    Box = box_styles:double(),
-
-    % okay so now we draw that table
-    table:draw_table(X,Y, Height, Fg,Bg, Box, [Width -1]),
-    ?TERMBOX:tb_print(X + 2, Y, Fg, Bg, FrameTitle),
-    ok.
+        table:draw_table(X, Y, Height - 1, Fg, Bg, Box, [Width - 2]),
+        ?TERMBOX:tb_print(X + 2, Y, Fg, Bg, FrameTitle);
+    true ->
+        ok
+    end.
