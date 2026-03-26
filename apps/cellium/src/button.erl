83c1a7f16d010bb5ce29095f7044b6fed3af145f
Wade Mealing <wmealing@gmail.com>
2026-03-26 21:37:57 +1000

    Fix layout.

--- apps/cellium/src/button.erl
+++ apps/cellium/src/button.erl
@@ -10,12 +10,14 @@ new(Id) ->
 
 -spec new(term(), string()) -> map().
 new(Id, Label) ->
-    (widget:new())#{id => Id,
-                    widget_type => button,
-                    label => Label,
-                    class => button,
-                    focusable => true,
-                    type => widget}.
+    (widget:new())#{
+        id => Id,
+        widget_type => button,
+        label => Label,
+        class => button,
+        focusable => true,
+        type => widget
+    }.
 
 -spec render(map()) -> ok.
 render(Widget) ->
@@ -25,28 +27,36 @@ render(Widget) ->
     Height = maps:get(height, Widget, 1),
     Width = maps:get(width, Widget, length(Label) + 2),
 
-    {FinalFg, FinalBg} = case HasFocus of
-        true -> {Bg, Fg}; % Invert colors for focus
-        false -> {Fg, Bg}
-    end,
-
-    if Height >= 3 ->
-        BoxStyle = if HasFocus -> box_styles:double(); true -> box_styles:square() end,
-        % Draw the frame
-        table:draw_table(X, Y, Height - 1, FinalFg, FinalBg, BoxStyle, [Width - 2]),
-        
-        % Print label centered
-        TextX = X + (Width - length(Label)) div 2,
-        TextY = Y + Height div 2,
-        ?TERMBOX:tb_print(TextX, TextY, FinalFg, FinalBg, Label),
-        ok;
-    true ->
-        FinalLabel = case HasFocus of
-            true -> "[" ++ Label ++ "]";
-            false -> " " ++ Label ++ " "
+    {FinalFg, FinalBg} =
+        case HasFocus of
+            % Invert colors for focus
+            true -> {Bg, Fg};
+            false -> {Fg, Bg}
         end,
-        ?TERMBOX:tb_print(X, Y, FinalFg, FinalBg, FinalLabel),
-        ok
+
+    if
+        Height >= 3 ->
+            BoxStyle =
+                if
+                    HasFocus -> box_styles:double();
+                    true -> box_styles:square()
+                end,
+            % Draw the frame
+            table:draw_table(X, Y, Height - 1, FinalFg, FinalBg, BoxStyle, [Width - 2]),
+
+            % Print label centered
+            TextX = X + (Width - length(Label)) div 2,
+            TextY = Y + Height div 2,
+            ?TERMBOX:tb_print(TextX, TextY, FinalFg, FinalBg, Label),
+            ok;
+        true ->
+            FinalLabel =
+                case HasFocus of
+                    true -> "[" ++ Label ++ "]";
+                    false -> " " ++ Label ++ " "
+                end,
+            ?TERMBOX:tb_print(X, Y, FinalFg, FinalBg, FinalLabel),
+            ok
     end.
 
 -spec render_focused(map()) -> ok.
